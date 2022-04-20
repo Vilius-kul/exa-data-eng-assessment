af2a2977989a8fca7ed121e5b16661545a4244b3
vilius <vilius.kulinas@gmail.com>
2022-04-20 23:04:06 +0100

    column names for one person

--- app/find_columns.py
+++ app/find_columns.py
@@ -1,18 +1,40 @@
 import json
 import os
 
+from fhir.resources.bundle import Bundle
+from fhir.resources.patient import Patient
+
 file_list = sorted(os.listdir("data"))
-# file_list.sort()
 
 file_name = "data/" + file_list[0]
-print(file_name)
-
 with open(file_name, "r") as f:
-    data = json.loads(f.read())
-
-print(data.keys())
+    data = json.load(f)
 
 
-print(data["resourceType"])
-print(data["type"])
-# print(data["entry"])
+one_bundle = Bundle.parse_obj(data)
+resources = []
+if one_bundle.entry is not None:
+    for entry in one_bundle.entry:
+        resources.append(entry.resource)  # type: ignore
+one_resource = []
+for j in range(len(resources)):
+    one_resource.append(type(resources[j]))
+
+# print(len(one_resource))
+
+uniq_resource = set(one_resource)
+# print(len(uniq_resource))
+# for i in uniq_resource:
+#     print(i)
+
+# print(type(resources[0]))
+
+one_patient = Patient.parse_obj(resources[0])
+
+# print(dir(one_patient))
+# print(one_patient.name[0])
+print(one_patient.id)
+print(one_patient.name[0].family)  # type: ignore
+print(one_patient.name[0].given[0])  # type: ignore
+print(one_patient.birthDate)
+print(one_patient.gender)
