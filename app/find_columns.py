a2d6b96a4e68dfd03567af504b8a08f489f5b4d6
vilius <vilius.kulinas@gmail.com>
2022-04-21 09:58:55 +0100

    table names from 1 record found

--- app/find_columns.py
+++ app/find_columns.py
@@ -8,33 +8,10 @@ file_list = sorted(os.listdir("data"))
 
 file_name = "data/" + file_list[0]
 with open(file_name, "r") as f:
-    data = json.load(f)
+    # data = json.load(f)
+    data = Bundle.parse_file(file_name)
 
-
-one_bundle = Bundle.parse_obj(data)
-resources = []
-if one_bundle.entry is not None:
-    for entry in one_bundle.entry:
-        resources.append(entry.resource)  # type: ignore
-one_resource = []
-for j in range(len(resources)):
-    one_resource.append(type(resources[j]))
-
-# print(len(one_resource))
-
-uniq_resource = set(one_resource)
-# print(len(uniq_resource))
-# for i in uniq_resource:
-#     print(i)
-
-# print(type(resources[0]))
-
-one_patient = Patient.parse_obj(resources[0])
-
-# print(dir(one_patient))
-# print(one_patient.name[0])
-print(one_patient.id)
-print(one_patient.name[0].family)  # type: ignore
-print(one_patient.name[0].given[0])  # type: ignore
-print(one_patient.birthDate)
-print(one_patient.gender)
+unique_resources = set()
+for item in data.entry:
+    unique_resources.add(item.resource.resource_type)  # type: ignore
+print(unique_resources)
