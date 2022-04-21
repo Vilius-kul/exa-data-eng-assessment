9c5a0246d688d6af7a9f99a982ba4599d0054e2a
vilius <vilius.kulinas@gmail.com>
2022-04-21 23:26:12 +0100

    somewhat filtered data

--- app/raw_json.py
+++ app/raw_json.py
@@ -11,17 +11,32 @@ f = open(file_name)
 data = json.load(f)
 
 
-# digging for fields
-# for i in data:
-#     print(i)
+# data["resourceType"] and  data["type"] --> str
 
-# print(data["resourceType"])
-# print(data["type"])
-# print(data["entry"])
-# print(type(data["entry"]))
+print(type(data["entry"]))
 # print(len(data["entry"]))
 # print(type(data["entry"][0]))
-print(data["entry"][0].keys())  # bingo
+# print(data["entry"][0].keys())  # bingo
+# print(data["entry"][0]["fullUrl"])  # full Url
+# print(data["entry"][0]["resource"].keys())
+
+
+# key value pairs from data["entry"][0]["resource"]
+# list_of_keys = data["entry"][0]["resource"].keys()
+# my_dict = data["entry"][0]["resource"]
+# for key in list_of_keys:
+#     print(f"Key =  {key} --->value = {my_dict[key]}")
+
+
+# all request per 1 person
+# for i in range(len(data["entry"])):
+#     print(data["entry"][i]["request"])
+
+
+# # all url's per 1 person
+# for i in range(len(data["entry"])):
+#     print(data["entry"][i]["fullUrl"])
+
 
 # Closing file
 f.close()
